82c7b7d82268c2e8b7d2637f5c8444e073463f91
Morgan McElroy <morganmcelroy@msn.com>
2026-06-07 14:35:57 -0700

    Convert Request Validation to own class

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Idea;
 use Illuminate\Http\Request;
-
+use App\Http\Requests\IdeaRequest;
 class IdeaController extends Controller
 {
     /**
@@ -31,9 +31,8 @@ class IdeaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(IdeaRequest $request)
     {
-        $request->validate(['description' => ['required', "min:10"]]);
         $idea = request()->description;
 
         Idea::create([
@@ -65,11 +64,11 @@ class IdeaController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Idea $idea)
+    public function update(IdeaRequest $request, Idea $idea)
     {
         $idea->update([
         "description" => request('idea'),
-        ]); //needs validation
+        ]);
 
         return redirect("/ideas/$idea->id");
     }
--- resources/views/ideas/edit.blade.php
+++ resources/views/ideas/edit.blade.php
@@ -7,13 +7,13 @@
       <div class="col-span-full">
           <label for="about" class="block text-sm/6 font-medium text-white">Edit your idea</label>
           <div class="mt-2">
-            <textarea id="about" name="idea" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6">{{ $idea->description }}</textarea>
-          </div>
+            <textarea id="description" name="description" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6">{{ $idea->description }}</textarea>
+            <x-forms.error name="description" />
+        </div>
         </div>
         <div class="mt-6 flex items-center gap-x-6">
             <button type="submit" class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Update</button>
         </div>
-
    </form>
      <div class="col-span-full mt-6">
         <form method="POST" action="/ideas/{{$idea->id}}">
